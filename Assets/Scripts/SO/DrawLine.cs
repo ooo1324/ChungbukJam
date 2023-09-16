efbf74f7269d1c5cfebb4e33c6e34d9e6455ee49
ooo1324 <ooo1324@daum.net>
2023-09-16 09:05:36 +0900

    Draw Line 미완, Shader 수정

---
+++ Assets/Scripts/SO/DrawLine.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawLine : MonoBehaviour
+{
+
+    public GameObject linePrefab;
+
+    [HideInInspector]
+    public Color lineColor;
+
+    LineRenderer lr;
+
+    EdgeCollider2D col;
+    List<Vector2> points = new List<Vector2>();
+
+    private void Start()
+    {
+        lineColor = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject go = Instantiate(linePrefab);
+            lr = go.GetComponent<LineRenderer>();
+            col = go.GetComponent<EdgeCollider2D>();
+            lr.startColor = lineColor;
+            lr.endColor = lineColor;
+            points.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            lr.positionCount = 1;
+            lr.SetPosition(0, points[0]);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            points.Add(pos);
+            lr.positionCount++;
+            lr.SetPosition(lr.positionCount - 1, pos);
+            col.points = points.ToArray();
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            points.Clear();
+        }
+    }
+}
--- Assets/Scripts/SO/Stamp.cs
+++ Assets/Scripts/SO/Stamp.cs
@@ -5,20 +5,8 @@ using UnityEngine;
 
 public class Stamp : MonoBehaviour
 {
-    bool isSelect = false;
+  
 
-    private void OnMouseDown()
-    {
-        if (!isSelect)
-        {
-            //클릭 안된 상태
-            isSelect = true;
 
-        }
-        else
-        {
-            //클릭 된 상태
-            isSelect = false;
-        }
-    }
+
 }
